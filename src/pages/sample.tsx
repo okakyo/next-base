09a199d104a6580246ba432c921bebaa2fd3e1b9
okakyo <oka@logly.co.jp>
2023-04-05 23:04:34 +0900

    First Commit

---
+++ src/pages/sample.tsx
@@ -0,0 +1,10 @@
+import {Box, Flex , Container, Form, Input, Checkbox} from "@chakra-ui/react"
+
+export default function SamplePage(){
+  return (
+  <Container>
+    <Box>
+    </Box>
+  </Container>
+  );
+}
